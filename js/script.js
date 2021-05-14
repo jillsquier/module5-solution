3f5fe8d64043499c6c5ac8e48111a8c98f9ada06
Jill Squier <77509810+jillsquier@users.noreply.github.com>
2021-05-14 03:03:43 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -102,15 +102,12 @@ function buildAndShowHomeHTML (categories) {
          },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
-           
+  
+//  {{randomCategoryShortName}} = 'chosenCategoryShortName'
       
-      //insertHtml ("#main-content", chosenCategoryShortName);
+ 
 
-             // menuItemsTitleHtml =
-    // insertProperty(menuItemsTitleHtml,
-                   // "name",
-                   // categoryMenuItems.category.name);
-        
+     
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
