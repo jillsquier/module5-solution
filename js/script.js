113315d1e38d37dc21480f57e4ee1504a59cf512
Jill Squier <77509810+jillsquier@users.noreply.github.com>
2021-08-02 12:40:12 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -62,56 +62,37 @@ var switchMenuToActive = function () {
 
 // On page load (before images or CSS)
 document.addEventListener("DOMContentLoaded", function (event) {
-
-// TODO: STEP 0: Look over the code from
-// *** start ***
-// to
-// *** finish ***
-// below.
-// We changed this code to retrieve all categories from the server instead of
-// simply requesting home HTML snippet. We now also have another function
-// called buildAndShowHomeHTML that will receive all the categories from the server
-// and process them: choose random category, retrieve home HTML snippet, insert that
-// random category into the home HTML snippet, and then insert that snippet into our
-// main page (index.html).
-//
-// TODO: STEP 1: Substitute [...] below with the *value* of the function buildAndShowHomeHTML,
-// so it can be called when server responds with the categories data.
-
-// *** start ***
-// On first load, show home view
 showLoading("#main-content");
-$ajaxUtils.sendGetRequest(
+  $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  homeHtml, // ***** <---- TODO: STEP 1: Substitute [...] ******
-  true); // Explicitly setting the flag to get JSON from server processed into an object literal
+  buildAndShowHomeHTML,
+  true);
 });
-// *** finish **
 
 
-// Builds HTML for the home page based on categories array
-// returned from the server.
 function buildAndShowHomeHTML (categories) {
-  // Load home snippet page
- $ajaxUtils.sendGetRequest(
+  $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
-    function (homeHtml) {
-      // Retrieve single category snippet
-      $ajaxUtils.sendGetRequest(
-        homeHtml,
-          function (chooseRandomCategory){
-             $ajaxUtils.sendGetRequest(
-                categories);
-      
-         },
-    false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
-}
-  
-//  {{randomCategoryShortName}} = 'chosenCategoryShortName'
-      
- 
+      function (homeHtml) {
+        chooseRandomCategory (categories);
+          insertHtml ("#main-content", chosenCategoryShortName);
+      },
+    false);
+  }
 
-     
+var insertProperty = function (html, randomCategoryShortName, chosenCategoryShortName) {
+var propToReplace = "{{" + {{randomCategoryShortName}} + "}}";
+string = string
+.replace(new RegExp(randomCategoryShortName, "chosenCategoryShortName"), chosenCategoryShortName);
+return string;
+}
+
+      
+function chooseRandomCategory (categories) {
+  var randomArrayIndex = Math.floor(Math.random() * categories.length);
+  return categories[randomArrayIndex];
+}
+
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
@@ -137,14 +118,19 @@ function buildAndShowHomeHTML (categories) {
       // of how to do that.
       // ....
 
- 
+     // False here because we are getting just regular HTML from the server, so no need to process JSON.
 
 
 
-function chooseRandomCategory (categories) {
-  var randomArrayIndex = Math.floor(Math.random() * categories.length);
-  return categories[randomArrayIndex];
-}
+
+// Given array of category objects, returns a random category object.
+//function chooseRandomCategory (categories) {
+  // Choose a random index into the array (from 0 inclusively until array length (exclusively))
+  //var randomArrayIndex = Math.floor(Math.random() * categories.length);
+
+  // return category object with that randomArrayIndex
+ // return categories[randomArrayIndex];
+//}
 
 
 // Load the menu categories view
